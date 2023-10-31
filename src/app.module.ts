a45651b7300cc4ead91e3679bd8c99ce006ad37d
Alfredo Palacios <joseaph.1998@gmail.com>
2023-10-30 23:04:12 -0600

    importación de modulo de archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/roles.guard';
+import { FileUploadsModule } from './file-uploads/file-uploads.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
     AuthModule,
     UsersModule,
+    FileUploadsModule,
   ],
   controllers: [AppController],
   providers: [
